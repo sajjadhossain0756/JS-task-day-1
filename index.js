8e6475ff04ab1027be2ebdce970d3530d4766a4a
sajjadhossain <sajjadhossain0756@gmail.com>
2024-08-28 23:27:36 +0600

    task-2 complete

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// task-1 first-day
+// task-1 start here first-day
 // I have 500 taka
 var totalMoney = 800;
 console.log("I give to shopkeeper total ="+ " " + totalMoney);
@@ -14,4 +14,25 @@ console.log("My total cost Is ="+ " " + totalCostIs);
 // I have money after total_cost
 var availableMoney = totalMoney - totalCostIs;
 console.log("Shokeeper will return to me ="+ " " + availableMoney);
+
+// task-2 start here
+// total subject is
+var totalSubject = 5;
+// I got Mathemathic subject
+var mathemathics =  70.100;
+// I got Biology subject
+var biology = 80;
+// I got Chemistry subject
+var chemistry = 60.3000;
+// I got Physics subject
+var physics = 65;
+// I got Bangle subject
+var bangla = 90;
+// I have got total numbers
+var totalNumbers = mathemathics + biology + chemistry + physics + bangla;
+console.log("I have got total Number =" + " " + totalNumbers);
+// I have got average number
+var averageNumber = totalNumbers / totalSubject;
+console.log("I have got average Number is =" + " " + averageNumber.toFixed(2));
+
 
