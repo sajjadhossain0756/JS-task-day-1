7ccc304084c042febe2ad9173fb79f851883c888
sajjadhossain <sajjadhossain0756@gmail.com>
2024-08-28 23:07:43 +0600

    task-1 complete

---
+++ index.js
@@ -0,0 +1,17 @@
+// task-1 first-day
+// I have 500 taka
+var totalMoney = 800;
+console.log("I give to shopkeeper total ="+ " " + totalMoney);
+// 1kg orange price is
+var orangePrice = 200;
+console.log("1kg orange Price Is ="+ " " + orangePrice);
+// 1kg apple price is
+var applePrice = 300;
+console.log("1kg apple Price Is ="+ " " + applePrice);
+// my total cost is after buying 1kg orange and 1kg apples
+var totalCostIs = orangePrice + applePrice;
+console.log("My total cost Is ="+ " " + totalCostIs);
+// I have money after total_cost
+var availableMoney = totalMoney - totalCostIs;
+console.log("Shokeeper will return to me ="+ " " + availableMoney);
+
